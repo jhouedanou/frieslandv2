5e587b18b953bf29457cd71cf961e9795388520d
Jean Luc Houédanou <jhouedanou@gmail.com>
2025-08-24 22:35:58 +0000

    Update Android build configuration to specify compile, min, and target SDK versions
    - Set compileSdk to 36, minSdk to 24, and targetSdk to 36 in build.gradle.kts for improved compatibility and performance.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.friesland.dashboard.friesland_dashboard"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     // NDK completely disabled
 
     compileOptions {
@@ -24,8 +24,9 @@ android {
         applicationId = "com.friesland.dashboard.friesland_dashboard"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
+        compileSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         
